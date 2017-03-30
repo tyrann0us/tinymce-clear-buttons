2709b53ae4407ddf3f7df7282b7b7e10a6359742
tyrann0us <pbammes@gmail.com>
2017-03-30 21:25:52 +0200

    Fixed text wrong plugin text domain, #fixes 1

--- admin/tinymce/tinymce-clear-float/languages/tinymce-clear-float-lang.php
+++ admin/tinymce/tinymce-clear-float/languages/tinymce-clear-float-lang.php
@@ -2,7 +2,7 @@
 
 $strings = 'tinyMCE.addI18n(
 	"' . _WP_Editors::$mce_locale . '.tinymce_clear_float", {
-		tooltip: "' . esc_js( __( 'Clear float', 'tinymce-clear-float' ) ) . '",
-		img_title: "' . esc_js( __( 'Clear float', 'tinymce-clear-float' ) ) . '",
+		tooltip: "' . esc_js( __( 'Clear float', 'tinymce-clear-buttons' ) ) . '",
+		img_title: "' . esc_js( __( 'Clear float', 'tinymce-clear-buttons' ) ) . '",
 	}
 )';
